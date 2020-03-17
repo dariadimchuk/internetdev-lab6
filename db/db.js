9f8616881a1b19c46f7de4bca1bf4c05416cf35e
DariaDimchuk <daria.dimchuk@gmail.com>
2020-03-16 23:12:42 -0700

    changed db.js

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({  
-    host: 'localhost',  
-    user: 'postgres',  
-    database: 'actors',  
-    password: 'admin',
+    host: 'ec2-52-45-14-227.compute-1.amazonaws.com',  
+    user: 'epagupynpixfzy',  
+    database: 'dep6krvenjdjrr',  
+    password: '800f53a88181f6fb30c4e1cbb650da363db10e6c9dfab75799bf750ffa00a928',
     port: 5432,
     ssl: false
 });  
